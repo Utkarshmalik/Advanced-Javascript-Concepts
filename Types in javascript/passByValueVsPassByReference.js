1f8c4cf37ecada515d2fd025296e93b6e45bb9d0
mohitverma6161 <utkarshmalik06@gmail.com>
2020-02-26 10:52:57 +0530

    updated

---
+++ Types in javascript/passByValueVsPassByReference.js
@@ -0,0 +1,13 @@
+//primitives types are passed by value
+
+var a = 5;
+var b = a;
+
+b++;
+
+console.log(a)
+console.log(b)
+
+//the change in b doesnot affect change in a - because primitive types are passd by value
+
+

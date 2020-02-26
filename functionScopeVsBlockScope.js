4784646e23a0b9e8a61808ab7d9a792b6a0c4ca9
mohitverma6161 <utkarshmalik06@gmail.com>
2020-02-26 10:15:30 +0530

    updated

--- functionScopeVsBlockScope.js
+++ functionScopeVsBlockScope.js
@@ -5,12 +5,9 @@
 Most of the languages are block scoped ,
 this means if you are enclosing somethging in a block , a new scope will startfrom there
 
-
 Javascript is functional scooped , it only creates a new scope if a new function is created
 
-
 let and const are used for block scoping
-
 
 */
 

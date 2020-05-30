6b778ff02c773f00f045b595fe77aff9f7359aca
mohitverma6161 <utkarshmalik06@gmail.com>
2020-05-30 17:15:27 +0530

    updated

--- CallApplyBind/CallApplyBind-2.js
+++ CallApplyBind/CallApplyBind-2.js
@@ -1,13 +1,30 @@
-function multiply(a, b) {
-  return a * b;
+// function multiply(a, b) {
+//   return a * b;
+// }
+
+
+// //This is what is callled as Function Currying :
+
+
+// const multiplyByTwo = multiply.bind(this, 2);
+// const multiplyByTen = multiply.bind(this, 10);
+
+// const x = multiplyByTwo(4)
+// const y = multiplyByTen(5);
+
+// console.log(x)
+// console.log(y)
+
+
+
+
+function add(a) {
+  return function (b) {
+    if (b == undefined)
+      return a;
+
+    return a + b;
+  }
 }
 
-const multiplyByTwo = multiply.bind(this, 2);
-const multiplyByTen = multiply.bind(this, 10);
-
-const x = multiplyByTwo(4)
-const y = multiplyByTen(5);
-
-console.log(x)
-console.log(y)
-
+console.log(add(2));
